53cc5768b0b4f87b97ec5895f5e4a2d120116261
Celi <hey.celinadigital@gmail.com>
2025-04-27 15:37:45 -0300

    Delete js/script.js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navList = document.querySelector('.nav-list');
-
-menuToggle.addEventListener('click', () => {
-  navList.classList.toggle('active');
-});
